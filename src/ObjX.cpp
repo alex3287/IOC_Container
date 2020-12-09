fde13990e69a3e477a7742ac0c84eab63ad2f05a
Alexander Mangazeev <alex3287@bk.ru>
2020-12-10 00:07:03 +0700

    made a separate class ObjX

---
+++ src/ObjX.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Александр Мангазеев on 09.12.2020.
+//
+
+#include <iostream>
+#include "ObjX.h"
+
+ObjX::ObjX() {}
+void ObjX::TestMethodX() {
+    std::cout << "This is ObjX" <<std::endl;
+}
---
+++ src/ObjX.h
@@ -0,0 +1,17 @@
+//
+// Created by Александр Мангазеев on 09.12.2020.
+//
+
+#ifndef IOC_CONTAINER_OBJX_H
+#define IOC_CONTAINER_OBJX_H
+
+#include "ISomeObjX.h"
+
+class ObjX: public  ISomeObjX {
+public:
+    ObjX();
+    void TestMethodX();
+};
+
+
+#endif //IOC_CONTAINER_OBJX_H
